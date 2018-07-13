61d8a680b91ade68b9b72e7b92ece8a7005b9f62
Jaejun Jang <jjj404001@gmail.com>
2018-07-13 18:29:18 +0900

    Add simple VBO, VAO, EBO prootypes.
    TODO : Implementation.

--- JJJGameEngine/JJJGameEngine/Shader/Triangle_Vertex.glsl
+++ JJJGameEngine/JJJGameEngine/Shader/Triangle_Vertex.glsl
@@ -1,10 +1,8 @@
 #version 400 core
+layout(location = 0) in vec3 position
 
 void main() 
 {
-    const vec4 vertices[3] = vec4[3](vec4(0.25, -0.25, 0.5, 1.0),
-                                     vec4(-0.25, -0.25, 0.5, 1.0),
-                                     vec4(0.25, 0.25, 0.5, 1.0));
-
-    gl_Position = vertices[gl_VertexID];
+    // gl_Position = vertices[gl_VertexID];
+    gl_Position = vec4(position, 1.0);
 }
---
+++ JJJGameEngine/JJJGraphicsEngine/Mesh.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+
+#include <gl/GL.h>
+#include <glext.h>
+#include <wglext.h>
+
+struct Stride
+{
+
+};
+
+class Mesh
+{
+	std::vector<Stride> vertices; // Collection of vertices position and color.
+
+
+	unsigned int number_of_vertex_ = 0;// number of vertecies.
+	unsigned int index_ = 0;           // Index in vao.
+
+	unsigned int primitive_ = GL_TRIANGLES;
+
+	GLuint VBO; // Vertex Buffer Object
+	GLuint VAO; // Vertex Attribute Object
+	GLuint EBO; // Element Buffer Object
+
+public:
+	void Initialize_VAO_VBO();
+	void Update_VAO_VBO();
+};
