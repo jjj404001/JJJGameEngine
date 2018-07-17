602f89d726edc3b1a119b708e08c494f73623eeb
Jaejun Jang <jjj404001@gmail.com>
2018-07-17 22:48:07 +0900

    Fix tessellaton shader bug about color.

--- JJJGameEngine/JJJEngine/Shader/Tessellation_evalution.glsl
+++ JJJGameEngine/JJJEngine/Shader/Tessellation_evalution.glsl
@@ -12,5 +12,8 @@ void main()
                    gl_TessCoord.z * gl_in[2].gl_Position);
 
 
-    color_for_GE = color_for_TE[0];
+    color_for_GE = vec4(0);
+    color_for_GE += gl_TessCoord.x * color_for_TE[0];
+    color_for_GE += gl_TessCoord.y * color_for_TE[1];
+    color_for_GE += gl_TessCoord.z * color_for_TE[2];
 }
